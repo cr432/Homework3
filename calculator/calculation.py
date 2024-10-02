aa4e30ba7eeb31618e3d84ec28a86c36e3c3c578
cramsterr <cr432@njit.edu>
2024-10-02 19:44:35 -0400

    calculator tweaks

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -2,8 +2,7 @@ class Calculation:
     def __init__(self, a, b, operation):
         self.a = a
         self.b = b
-        self.operation = operation  # Store the operation function
+        self.operation = operation
 
     def get_result(self):
-        # Call the stored operation with a and b
         return self.operation(self.a, self.b)
